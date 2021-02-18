51d90264aee72135f6bba9ac55b1aba0fede5922
Toci <tocirml2@gmail.com>
2021-02-18 09:50:04 -0600

    Add express server config

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// create an express app
+const express = require('express');
+const app = express();
+
+// use the express-static middleware
+app.use(express.static('src'));
+
+// define the first route
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World!</h1>');
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Server is running on http://localhost:3000')
+);
